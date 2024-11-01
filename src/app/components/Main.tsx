768e5ead389dfda8d4a4395fa2078dbcc992a39a
Naif Otibi <nn.n@outlook.cl>
2024-11-01 14:15:28 +0300

    adjusting a lot of thing and adding the ref

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,15 +1,19 @@
+"use client"
+
+import { useRef, useState } from "react";
 import { Faq, FaqAccordion, FaqHeader } from "./Faq";
 
 
 
 
-
 export default function MainComponent(){
+    const [expand , setExpand] = useState<boolean>(false);
+    const containerRef = useRef(null);
     return (
         <Faq>
             <FaqHeader title="FAQs" />
             <div className="flex flex-col gap-3 divide-y">
-                <FaqAccordion id={1} header="Lorem ipsum dolor sit amet consectetur adipisicing?">
+                <FaqAccordion containerRef={containerRef} id={1} header="Lorem ipsum dolor sit amet consectetur adipisicing?" expanded={expand} onExpandedChange={() => setExpand(!expand)}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quas commodi cum suscipit adipisci eius placeat voluptatem est ipsum recusandae enim, ratione debitis consectetur numquam molestiae quae iusto nostrum obcaecati.
                 </FaqAccordion>
             </div>
