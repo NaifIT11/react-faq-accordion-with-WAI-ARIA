dab4630eee638f23fbd92483279edffb838ae7c3
Naif Otibi <nn.n@outlook.cl>
2024-11-01 13:33:33 +0300

    developing faq header component

--- src/app/components/Faq.tsx
+++ src/app/components/Faq.tsx
@@ -1,5 +1,15 @@
+import { Sparkle } from "lucide-react";
 
 
 
+export 
 
 
+export function FaqHeader({title}: {title: string}){
+    return (
+        <div className="flex gap-4">
+            <Sparkle className="size-6 text-rose-300" />
+            <h1 className="text-2xl text-rose-900">{title}</h1>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import MainComponent from "./components/Main";
 
 export default function Home() {
   return (
-    <MainComponent />
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <MainComponent />
+    </div>
   )
 }
